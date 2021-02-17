cc68c42065291d7cbed734dec1f880b68e413dca
Korsakov Pavel <pavelkpv@gmail.com>
2021-02-17 22:28:25 +0700

    add models

--- content/models.py
+++ content/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='posts')
+    image = models.ImageField(upload_to='posts/', null=True,
+                              blank=True)  # поле для картинки
+
+    def __str__(self):
+        return self.text
