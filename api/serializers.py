e4cde8ab14ea7ec5fb1b219747724d175ac023da
Korsakov Pavel <pavelkpv@gmail.com>
2021-02-18 05:55:45 +0700

    v0.04 afrer linters

--- api/serializers.py
+++ api/serializers.py
@@ -1,21 +1,58 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
+from rest_framework.serializers import raise_errors_on_nested_writes
+from rest_framework.utils import model_meta
 
-from content.models import Post
+from content.models import Post, Image
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ['image']
+        model = Image
 
 
 class PostsSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(read_only=True,
                                           slug_field='username')
-
-    def validate(self, data):
-        image = data.get('image')
-        if image:
-            if image.size > 200*1024:
-                raise ValidationError("Image file too large ( > 200kb )")
-        return data
+    post_images = ImageSerializer(read_only=True, many=True)
 
     class Meta:
-        fields = '__all__'
+        fields = ['id', 'text', 'pub_date', 'author', 'post_images']
         model = Post
         read_only_fields = ['pub_date']
+
+    def create(self, validated_data):
+        post = Post.objects.create(**validated_data)
+        files = self.context['request'].FILES.getlist('post_images')
+        for file in files:
+            Image.objects.create(post=post, image=file)
+        return post
+
+    def update(self, instance, validated_data):
+        Image.objects.filter(post=instance).delete()
+        files = self.context['request'].FILES.getlist('post_images')
+        for file in files:
+            Image.objects.create(post=instance, image=file)
+        raise_errors_on_nested_writes('update', self, validated_data)
+        info = model_meta.get_field_info(instance)
+        m2m_fields = []
+        for attr, value in validated_data.items():
+            if attr in info.relations and info.relations[attr].to_many:
+                m2m_fields.append((attr, value))
+            else:
+                setattr(instance, attr, value)
+        instance.save()
+        for attr, value in m2m_fields:
+            field = getattr(instance, attr)
+            field.set(value)
+
+        return instance
+
+    def validate(self, data):
+        files = self.context['request'].FILES.getlist('post_images')
+        for file in files:
+            if file.size > 200 * 1024:
+                raise ValidationError(
+                    "Image file '{}' too large ( > 200kb )".format(file.name))
+        return data
--- content/models.py
+++ content/models.py
@@ -6,10 +6,16 @@ from users.models import User
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
                                related_name='posts')
-    image = models.ImageField(upload_to='posts/', null=True,
-                              blank=True)  # поле для картинки
 
     def __str__(self):
         return self.text
+
+
+class Image(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='post_images')
+    image = models.ImageField(upload_to='posts/')
