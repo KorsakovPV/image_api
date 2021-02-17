ac87c59c311d2981da10223462fb61c4c540f37f
Korsakov Pavel <pavelkpv@gmail.com>
2021-02-18 01:03:53 +0700

    v0.03 complete

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from content.models import Post
 
@@ -6,6 +7,13 @@ from content.models import Post
 class PostsSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(read_only=True,
                                           slug_field='username')
+
+    def validate(self, data):
+        image = data.get('image')
+        if image:
+            if image.size > 200*1024:
+                raise ValidationError("Image file too large ( > 200kb )")
+        return data
 
     class Meta:
         fields = '__all__'
