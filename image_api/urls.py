c0e9f0bea39252ce11e810aa6f289a445428a267
Korsakov Pavel <pavelkpv@gmail.com>
2021-02-22 00:34:42 +0700

    v0.04 add openapi

--- image_api/urls.py
+++ image_api/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import TemplateView
 
 from rest_framework.schemas import get_schema_view
 
@@ -9,7 +9,7 @@ urlpatterns = [
     # Use the `get_schema_view()` helper to add a `SchemaView` to project URLs.
     #   * `title` and `description` parameters are passed to `SchemaGenerator`.
     #   * Provide view name for use with `reverse()`.
-    path('openapi', get_schema_view(
+    path('openapi/', get_schema_view(
         title="Your Project",
         description="API for all things …",
         version="1.0.0"
@@ -19,5 +19,7 @@ urlpatterns = [
 
 urlpatterns += [
     path('admin/', admin.site.urls),
+    path('redoc/', TemplateView.as_view(template_name='redoc.html'),
+         name='redoc'),
     path('api/', include('api.urls')),
 ]
