54a57eb67b99c5aafbd1f0fbb6ac63fa21019020
Korsakov Pavel <pavelkpv@gmail.com>
2021-02-23 16:22:16 +0700

    v0.04 add unittest v0.01

--- api/tests/test_urls.py
+++ api/tests/test_urls.py
@@ -1,9 +1,17 @@
+import base64
+import json
+
 from django.test import Client, TestCase
-from django.urls import reverse
+from django.urls import reverse, path, include
 import factory
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+
+from image_api.settings import BASE_DIR
 from users.models import User
 from content.models import Post, Image
-from rest_framework.test import APIRequestFactory, APIClient
+from rest_framework.test import APIRequestFactory, APIClient, APITestCase, \
+    URLPatternsTestCase
 
 
 class UserFactory(factory.Factory):
@@ -18,7 +26,7 @@ class UserFactory(factory.Factory):
 
         model = User
 
-    username = 'Test user'
+    username = 'Test_user'
     email = 'test@test.test'
     password = '12345six'
     first_name = 'Test user first_name'
@@ -31,28 +39,12 @@ def _create_user(**kwargs):
     return user
 
 
-class StaticURLTests(TestCase):
+class URLTests(TestCase):
     def setUp(self):
         self.guest_client = Client()
         self.factory = APIRequestFactory()
         self.client = APIClient()
         self.user = _create_user()
-        # self.authorized_client = Client()
-
-    # redoc
-    # createAuthToken post
-    # posts-list get
-    # post-create post
-    # post-retrieve get
-    # post-update put
-    # post-destroy del
-    # post-partialUpdate patch
-
-    # Отправляем запрос через client,
-    # созданный в setUp()
-    # url_names = {'redoc': '',
-    #              'get_token': '',
-    #              }
 
     def test_redoc_page(self):
         url_redoc = reverse('redoc')
@@ -62,25 +54,94 @@ class StaticURLTests(TestCase):
                              url_redoc))
 
 
-    # TODO переделать тест аутентификации
-    def test_authtoken_url(self):
-        url_authtoken = reverse('get_token')
-        response = self.guest_client.post(url_authtoken)
-        self.assertEqual(response.status_code, 400,
-                         msg='Ресурс {} без переданных обязательных полей '
-                             'должен выдавать ошибку 400.'.format(
-                             url_authtoken))
-        response = self.client.post(url_authtoken,
-                                          {
-                                              "username": self.user.username,
-                                              "password": self.user.password
-                                          },
-                                          format='json')
-        response2 = self.factory.post(url_authtoken,
-                                          {
-                                              "username": self.user.username,
-                                              "password": self.user.password
-                                          },
-                                          format='json')
+class PostTests(APITestCase):
 
-        print(10)
+    def setUp(self):
+        self.client_authentication = APIClient()
+        self.client_no_authentication = APIClient()
+        self.user = _create_user()
+        self.token = Token.objects.create(user=self.user).key
+        self.client_authentication.credentials(
+            HTTP_AUTHORIZATION='Token ' + self.token)
+        self.path_img = f'{BASE_DIR}/api/tests/test_data/'
+
+    def test_post_create(self):
+        url = reverse('posts-list')
+        data = {
+            'text': 'test post',
+        }
+        response = self.client_authentication.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        post = Post.objects.last()
+        self.assertEqual(post.text, 'test post')
+
+    def test_posts_list(self):
+        url = reverse('posts-list')
+        response = self.client_no_authentication.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        response = self.client_authentication.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_posts_retrieve(self):
+        post = Post.objects.get_or_create(author=self.user, text='test post')[
+            0]
+        url = reverse('posts-detail', kwargs={'pk': post.id})
+        response = self.client_no_authentication.get(url, format='json')
+        self.assertEqual(response.status_code,
+                         status.HTTP_401_UNAUTHORIZED)
+        response = self.client_authentication.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get('text'), post.text)
+        self.assertEqual(response.data.get('author'), post.author.username)
+
+    def test_post_update(self):
+        post = Post.objects.get_or_create(author=self.user, text='test post')[
+            0]
+        url = reverse('posts-detail', kwargs={'pk': post.id})
+        response = self.client_no_authentication.put(url, format='json')
+        self.assertEqual(response.status_code,
+                         status.HTTP_401_UNAUTHORIZED)
+        data = {'text': 'test post put', }
+        response = self.client_authentication.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get('text'), data.get('text'))
+
+    def test_post_partial_update(self):
+        post = Post.objects.get_or_create(author=self.user, text='test post')[
+            0]
+        url = reverse('posts-detail', kwargs={'pk': post.id})
+        response = self.client_no_authentication.patch(url, format='json')
+        self.assertEqual(response.status_code,
+                         status.HTTP_401_UNAUTHORIZED)
+        data = {'text': 'test post put', }
+        response = self.client_authentication.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get('text'), data.get('text'))
+
+    def test_post_partial_destroy(self):
+        post = Post.objects.get_or_create(author=self.user, text='test post')[
+            0]
+        url = reverse('posts-detail', kwargs={'pk': post.id})
+        response = self.client_no_authentication.delete(url, format='json')
+        self.assertEqual(response.status_code,
+                         status.HTTP_401_UNAUTHORIZED)
+        response = self.client_authentication.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    # TODO Доделать тест. Тест не сохраняет картинку.
+    def test_post_image(self):
+        url = reverse('posts-list')
+        image = f'{self.path_img}test_image_2.jpg'
+        with open(image, 'rb') as img:
+            image_bytes = img.read()
+        im_b64 = base64.b64encode(image_bytes).decode("utf8")
+        headers = {
+            'Content-type': 'multipart/form-data; boundary=<calculated when request is sent>',
+            'Accept': '*/*'}
+        data = {
+            'text': 'test post',
+            'post_images': [im_b64],
+        }
+        response = self.client_authentication.post(url, data=data,
+                                                   headers=headers,
+                                                   format='json')
